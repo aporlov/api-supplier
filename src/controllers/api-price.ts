30fbe47d3a62f8c4c7a9aeee43802b117cd175de
Orlov Alexey <aporlov@gmail.com>
2016-06-24 12:18:40 +0300

    err.toStrimg()

--- src/controllers/api-price.ts
+++ src/controllers/api-price.ts
@@ -12,10 +12,10 @@ router.post('/',(req, res)=>{
  let price = new Price(req.body);
  Price.findOneAndUpdate( {code:price.code, supplier_inn:price.supplier_inn, price_name:price.price_name},price,{upsert:true},(err)=>{
  if (err) {
-         res.json({success: false , msg: 'Error '});
+         res.json({success: false , msg: err.toString()});
       } else {
          res.json({success: true , msg: 'Successful created price'});
-      }
+      } 
  });
 });
 /*POST for creating account. For test only 
